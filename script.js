10dd2559439f4a6fdd6363c0bcf6cf14814f524a
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-25 13:26:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function change(){
+     const bold = document.getElementById("status");
+   
+     let currentFontSize = getComputedStyle(bold).fontSize;
+
+     if(currentFontSize == "16px"){
+         bold.textContent = "Entered Metaverse",bold.style.fontSize = "30px";
+     }
+     else{
+         bold.style.fontSize = "16px";
+     }
+ }
