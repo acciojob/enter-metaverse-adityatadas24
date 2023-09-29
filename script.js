82102a63d69bb90dc41fe14323bcdcbd1e69cbfe
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-29 19:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const para = document.getElementById("status");
 const button = document.getElementById("enterBtn");
 
+function changeString() {
+	 para.innerHTML = "<h1>Entered Metaverse</h1>";
+	
+}
 
-document.addEventListener("click", () => {
-    para.innerHTML = "<h1>Entered Metaverse</h1>";
+// document.addEventListener("click", () => {
+   
 
-});
+// });
